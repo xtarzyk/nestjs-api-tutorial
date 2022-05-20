83aef77867dc856d66ccf6a9d008f7aaeb32154e
xtarzyk <xtarzyk@tutanota.com>
2022-05-20 14:24:48 +0200

    init

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthContoller } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  controllers: [AuthContoller],
+  providers: [AuthService],
+})
+export class AuthModule {}
